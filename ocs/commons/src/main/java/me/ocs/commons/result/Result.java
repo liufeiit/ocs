eecdef3c0dbf7e13fb0e3561429e826a892f9470
liufei_it <liufei_it@126.com>
2015-02-28 18:39:58 +0800

    第一次提交

---
+++ ocs/commons/src/main/java/me/ocs/commons/result/Result.java
@@ -0,0 +1,117 @@
+package me.ocs.commons.result;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 表示一个服务返回结集.
+ * 
+ * @author 刘飞
+ * 
+ * @version 1.0.0
+ * @since 2015年2月28日 下午5:45:20
+ */
+public class Result {
+
+	private boolean success;
+	
+	private String message;
+	
+	private long resultCode;
+	
+	private Object response;
+	
+	private Map<String, Object> data;
+	
+	public Result() {
+		this(false);
+	}
+
+	public Result(boolean success) {
+		super();
+		this.success = success;
+	}
+	
+	public static Result newError() {
+		return new Result();
+	}
+	
+	public static Result newSuccess() {
+		return new Result(true);
+	}
+	
+	public Result response(Object response) {
+		this.response = response;
+		return this;
+	}
+	
+	public <T> T getResponse(Class<T> type) {
+		return type.cast(response);
+	}
+	
+	public Result data(Map<String, Object> data) {
+		if(this.data == null) {
+			this.data = new HashMap<String, Object>();
+		}
+		this.data.putAll(data);
+		return this;
+	}
+
+	public Result data(String key, Object value) {
+		if(data == null) {
+			data = new HashMap<String, Object>();
+		}
+		data.put(key, value);
+		return this;
+	}
+	
+	public <T> T getData(String key, Class<T> type) {
+		if(data == null) {
+			return null;
+		}
+		return type.cast(data.get(key));
+	}
+	
+	public Map<String, Object> getData() {
+		return data;
+	}
+
+	public Result success(boolean success) {
+		setSuccess(success);
+		return this;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public Result message(String message) {
+		setMessage(message);
+		return this;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Result resultCode(long resultCode) {
+		setResultCode(resultCode);
+		return this;
+	}
+
+	public long getResultCode() {
+		return resultCode;
+	}
+
+	public void setResultCode(long resultCode) {
+		this.resultCode = resultCode;
+	}
+}
---
+++ ocs/commons/src/main/java/me/ocs/commons/result/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @author 刘飞
+ * 
+ * @version 1.0.0
+ * @since 2015年2月28日 下午5:44:18
+ */
+package me.ocs.commons.result;
