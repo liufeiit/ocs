4166f62fbb631e5151db0bf361875e6b0a3ba3b0
liufeiit <liufei_it@126.com>
2015-05-16 17:35:57 +0800

    11

--- ocs/comm/src/test/java/commons/EncryptorsTest.java
+++ ocs/comm/src/test/java/commons/EncryptorsTest.java
@@ -12,6 +12,14 @@ import me.ocs.commons.crypto.password.PasswordEncoder;
 public class EncryptorsTest {
 
 	public static void main(String[] args) {
+		Integer a = 1;
+		Integer b = 1;
+		Long c = 1L;
+		System.out.println(a == b);
+		System.out.println(c.equals(a));
+	}
+
+	public static void main0(String[] args) {
 		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		System.out.println(passwordEncoder.encode("liufei123456"));
 		System.out.println(passwordEncoder.matches("liufei123456", passwordEncoder.encode("liufei123456")));
